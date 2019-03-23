9ba1470c4ac6b7962045b5c24a67ef9b2cde1d82
Ajin Jude <Ajin Jude>
2019-03-24 03:43:23 +0530

    Cleanup

--- lib/seven_segment.rb
+++ lib/seven_segment.rb
@@ -1,29 +1,32 @@
+require 'pry'
 class SevenSegment
-  @keys = {
-      "." => "       . ",
-      "-" => "    __   ",
-      "0" => " _ | ||_|",
-      "1" => "     |  |",
-      "2" => " _  _||_ ",
-      "3" => " _  _| _|",
-      "4" => "   |_|  |",
-      "5" => " _ |_  _|",
-      "6" => " _ |_ |_|",
-      "7" => " _   |  |",
-      "8" => " _ |_||_|",
-      "9" => " _ |_| _|"
+  @keys =
+    {
+      '.' => '       . ',
+      '-' => '    __   ',
+      '0' => ' _ | ||_|',
+      '1' => '     |  |',
+      '2' => ' _  _||_ ',
+      '3' => ' _  _| _|',
+      '4' => '   |_|  |',
+      '5' => ' _ |_  _|',
+      '6' => ' _ |_ |_|',
+      '7' => ' _   |  |',
+      '8' => ' _ |_||_|',
+      '9' => ' _ |_| _|'
     }
 
   def self.to_numbers(segment)
-    lines = segment.split(/\n/).map{|line| line.scan(/.../)}
-    number = lines.transpose.map{|n| @keys.key(n.join)}.join.to_f
-    number % 1 == 0 ? number.to_i : number
+    lines = segment.each_line.map { |line| line.scan(/.../) }
+    number = lines.transpose.map { |n| @keys.key(n.join) }.join.to_f
+    (number % 1).zero? ? number.to_i : number
   end
 
   def self.to_segment(number)
-    raise ArgumentError,"Characters not supported" if (number.to_s.scan(/[A-Z]/).any? || number.to_s.scan(/[a-z]/).any?)
+    raise ArgumentError, 'Characters not supported' if (number.to_s.scan(/[A-Z]/).any? || number.to_s.scan(/[a-z]/).any?)
+
     segment = number.to_s.chars.map { |n| @keys[n].scan(/.../) }
-    segment.transpose.map { |s| s.join }.join("\n")
+    segment.transpose.map(&:join).join("\n")
   end
 
   class << self
